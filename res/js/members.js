12bfbbd3a0309b7c794444090c169f09552f8ed7
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-08-05 14:27:33 +0600

    Fix the fetching for `0th` elem
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- res/js/members.js
+++ res/js/members.js
@@ -1,37 +1,48 @@
 // Fetch GitHub Org public members list
 
-var newDiv = document.createElement("div");
-newDiv.classList.add("col-4");
-var devCard = document.getElementById('devCard');
-var clonedDiv = devCard.cloneNode(true);
-// Append the elems to the DOM
-newDiv.appendChild(clonedDiv);
-document.getElementById('cardsHolder').appendChild(newDiv);
-
-
 // Fetch the data
-/* fetch('https://api.github.com/orgs/nodejs/members')
+fetch('https://api.github.com/orgs/nodejs/members')
 	.then(res => res.json())
 	.then(data => {
-		console.log(data);
-		// var membersAvatar = document.getElementById('members').innerHTML = "<img src='" + data[0]["avatar_url"] + "'>"
-		var membersAvatar = document.getElementById('members');
+		// console.log(data);
+		var memAvatar = document.getElementById('members');
 		var devName = document.getElementById('devName');
-		var devLink1 = document.getElementById('devLink1');
+		var devLink = document.getElementById('devLink');
+
+		// Add for the first elem
+		memAvatar.src = data[0]["avatar_url"];
+		devName.textContent = data[0]["login"];
+		devLink.href = data[0]["html_url"];
 
 
+		for (var i = 1; i <= 20; i++) {
+			var newDiv = document.createElement("div");
+			newDiv.classList.add("col-4");
+			var devCard = document.getElementById('devCard');
+			var clonedDiv = devCard.cloneNode(true);
+			// Append the elems to the DOM
+			newDiv.appendChild(clonedDiv);
+			document.getElementById('cardsHolder').appendChild(newDiv);
 
+			// Image 
+			memAvatar.id = "memImage" + i;
+			var newMemberAvatar = document.getElementById("memImage" + i);
+			newMemberAvatar.src = data[i]["avatar_url"];
 
-		for (i = 0; i < 3; i++) {
-			membersAvatar.cloneNode()
-			membersAvatar.src = data[i]["avatar_url"];
-			devName.textContent = data[i]["login"];
-			devLink1.href = data[i]["html_url"];
+			// Dev Name
+			devName.id = "devName" + i;
+			var newDevName = document.getElementById("devName" + i);
+			newDevName.textContent = data[i]["login"];
+
+			//Github profile link
+			devLink.id = "devLink" + i;
+			var newDevLink = document.getElementById('devLink' + i);
+			newDevLink.href = data[i]["html_url"];
 		}
 
 
 	})
-	.catch(err => console.error(err)) */
+	.catch(err => console.error(err))
 
 
 
