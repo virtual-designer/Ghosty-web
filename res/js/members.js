c2daa4201d9c0ea0b16bbd7fa9599f44e5cadce0
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2022-08-05 14:52:49 +0600

    Addition of members page (#4)
    * Create members.html file
    
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
    
    * Fix navbar link for Members & add missing favicons
    
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
    
    * Create working fetching of org members and nicely display on members page
    
    NOTE: Currently it's only working for first member.
    TODO: A loop has to be implemented
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
    
    * Add code for dynamic div creation
    
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
    
    * Fix the fetching for `0th` elem
    
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
    
    * Add bottom margin to all the new div holding the card
    
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
    
    * Finalize the `members.js` API fetching and displaying
    
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
    
    * Add mobile responsiveness for the card divs
    
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
    
    * Make the cards full width on mobile screens
    
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

---
+++ res/js/members.js
@@ -0,0 +1,54 @@
+// Fetch GitHub Org public members list
+
+const orgName = "GhostyORG";
+
+// Fetch the data
+fetch('https://api.github.com/orgs/' + orgName + '/members')
+	.then(res => res.json())
+	.then(data => {
+		// console.log(data);
+		var memAvatar = document.getElementById('members');
+		var devName = document.getElementById('devName');
+		var devLink = document.getElementById('devLink');
+
+		// Add for the first elem
+		memAvatar.src = data[0]["avatar_url"];
+		devName.textContent = data[0]["login"];
+		devLink.href = data[0]["html_url"];
+
+		for (var i = 1; i <= Object.keys(data).length; i++) {
+			var newDiv = document.createElement("div");
+			newDiv.classList.add("col-lg-4");
+			newDiv.classList.add("col-sm-12");
+			newDiv.classList.add("col-md-6");
+
+			var devCard = document.getElementById('devCard');
+			var clonedDiv = devCard.cloneNode(true);
+			// Append the elems to the DOM
+			newDiv.appendChild(clonedDiv);
+			document.getElementById('cardsHolder').appendChild(newDiv);
+
+			// Image 
+			memAvatar.id = "memImage" + i;
+			var newMemberAvatar = document.getElementById("memImage" + i);
+			newMemberAvatar.src = data[i]["avatar_url"];
+
+			// Dev Name
+			devName.id = "devName" + i;
+			var newDevName = document.getElementById("devName" + i);
+			newDevName.textContent = data[i]["login"];
+
+			//Github profile link
+			devLink.id = "devLink" + i;
+			var newDevLink = document.getElementById('devLink' + i);
+			newDevLink.href = data[i]["html_url"];
+		}
+	})
+	.catch(err => console.error(err))
+
+
+
+
+
+
+
