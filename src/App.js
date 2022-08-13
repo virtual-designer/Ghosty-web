a4593e16332be86c0ab7de592f8b8697bfa3f6ab
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-08-14 00:30:38 +0600

    Add header and footer under browser router
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import Projects from "./pages/projects";
 import Members from "./pages/members";
 // Stylessheets
 import "./styles/css/custom/index.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/members" element={<Members />} />
           <Route path="/about" element={<About />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
