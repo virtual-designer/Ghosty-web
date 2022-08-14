c322c091ff157b0be8ac53eb4c81622379d258e0
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-08-14 13:08:31 +0600

    Add new fetching code for projects using octokit REST client
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Octokit } from "@octokit/rest";
 
 export default function Projects() {
   return (
@@ -16,3 +17,34 @@ export default function Projects() {
     </div>
   );
 }
+
+const octokit = new Octokit();
+
+// Compare: https://docs.github.com/en/rest/reference/repos/#list-organization-repositories
+octokit.rest.repos
+  .listForOrg({
+    org: "GhostyORG",
+    type: "public",
+  })
+  .then(({ data }) => {
+    // handle data
+    console.log(data);
+    var d = document,
+      main = d.getElementById("orgProjects"),
+      ul = d.createElement("ul");
+
+    main.appendChild(ul);
+
+    data.forEach((element) => {
+      var link = d.createElement("a");
+      link.textContent = element["name"];
+      link.setAttribute("href", element["html_url"]);
+      var li = d.createElement("li");
+      var des = d.createElement("div");
+      des.textContent = element["description"];
+
+      li.appendChild(link);
+      li.appendChild(des);
+      ul.appendChild(li);
+    });
+  });
