329f3f9037fb37d240e1bb845b503286a381153a
Ar Rakin <rakinar2@gmail.com>
2022-10-03 15:01:11 +0000

    feat: add user badges

--- src/scripts/js/members.js
+++ src/scripts/js/members.js
@@ -1,5 +1,50 @@
 // Fetch GitHub Org public members list
 
+const memberData = [
+  {
+      login: "Abir-Tx",
+      role: "Lead Developer"
+  },
+  {
+      login: "killerclaws12",
+      role: "CEO"
+  },
+  {
+      login: "phyouthcenter1",
+      role: "Developer"
+  },
+  {
+      login: "TyEggers",
+      role: "Developer"
+  },
+  {
+      login: "virtual-designer",
+      role: "Developer"
+  }
+];
+
+const colors = { "CEO": "text-bg-primary", "Developer": "text-bg-danger", "Lead Developer": 'text-bg-success' };
+
+const findRole = login => {
+  for (const member of memberData) {
+    if (member.login === login) {
+      return member.role;
+    }
+  }
+
+  return null;
+};
+
+const findColor = role => {
+  for (const r in colors) {
+    if (role === r) {
+      return colors[r];
+    }
+  }
+
+  return null;
+};
+
 const orgName = "GhostyORG";
 
 // Fetch the data
@@ -11,11 +56,13 @@ export function membersFetch() {
       var memAvatar = document.getElementById("members");
       var devName = document.getElementById("devName");
       var devLink = document.getElementById("devLink");
+      const devRole = document.getElementById("devRole");
 
       // Add for the first elem
       memAvatar.src = data[0]["avatar_url"];
       devName.textContent = data[0]["login"];
       devLink.href = data[0]["html_url"];
+      devRole.href = findRole(data[0]["login"]);
 
       for (var i = 1; i <= Object.keys(data).length - 1; i++) {
         var newDiv = document.createElement("div");
@@ -43,6 +90,13 @@ export function membersFetch() {
         devLink.id = "devLink" + i;
         var newDevLink = document.getElementById("devLink" + i);
         newDevLink.href = data[i]["html_url"];
+
+        // Dev Role
+        devRole.id = "devRole" + i;
+        const newDevRole = document.getElementById("devRole" + i);
+        const role = findRole(data[i]["login"]);
+        newDevRole.textContent = role;
+        newDevRole.classList.add(findColor(role));
       }
     })
     .catch((err) => console.error(err));
